f53d5360541b242d71519dd1e29b791ac5230820
Petr Mlčoch <petr@mlcochovi.cz>
2022-10-06 15:35:48 +0200

    found out the secondary_top.css mystery + refactor manifest to use same comment style

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,9 @@
-//= link_tree ../images
-
-//= build separate css file for each /app/assets/stylesheets/*.css file
-//= link_directory ../stylesheets .css
-
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+/*
+ *= link_tree ../images
+ *
+ * build separate css file for each /app/assets/stylesheets/*.css file
+ *= link_directory ../stylesheets .css
+ *
+ *= link_tree ../../javascript .js
+ *= link_tree ../../../vendor/javascript .js
+ */
---
+++ public/assets/manifest-8e6fe3715e7668154050b27040002dbbde5f4a105262e0f8a89ea22454fffd3e.js
@@ -0,0 +1,9 @@
+/*
+
+ *
+ * build separate css file for each /app/assets/stylesheets/*.css file
+
+ *
+
+
+ */;
